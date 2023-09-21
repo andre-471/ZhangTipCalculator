826f057797370c8697d6f032bec10cbf8bc35954
BT_1N3_02 <sample@bths.edu>
2023-09-21 10:58:34 -0400

    implemented multiple calculation

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,37 +3,45 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        double totalCost = 0.0;
+        double[] totalOfEachMenu = new double[5];
+        do {
+            double totalCost = 0.0;
 
-        System.out.print("How many people are in your group: ");
-        int numPeople = scan.nextInt();
-        System.out.print("What's the tip percentage? (0-100): ");
-        int tipPercent = scan.nextInt();
-        scan.nextLine();
-
-        double aCost = 0.0;
-        while (aCost != -1) {
-            totalCost += aCost;
-            System.out.print("Enter a cost in dollars and cents (-1 to end): ");
-            aCost = scan.nextDouble();
+            System.out.print("How many people are in your group: ");
+            int numPeople = scan.nextInt();
+            System.out.print("What's the tip percentage? (0-100): ");
+            int tipPercent = scan.nextInt();
             scan.nextLine();
-        }
 
-        // cost is cost of food w/o tip
-        // bill is cost + tip
-        double costPerPerson = totalCost / numPeople;
-        double tipAmount = totalCost * tipPercent / 100;
-        double totalBill = tipAmount + totalCost;
-        double tipPerPerson = tipAmount / numPeople;
-        double billPerPerson = costPerPerson + tipPerPerson;
+            double aCost = 0.0;
+            while (aCost != -1) {
+                totalCost += aCost;
+                System.out.print("Enter a cost in dollars and cents (-1 to end): ");
+                aCost = scan.nextDouble();
+                scan.nextLine();
+            }
 
-        // https://stackoverflow.com/questions/2538787/how-to-print-a-float-with-2-decimal-places-in-java
-        System.out.println("Total bill before tip: " + String.format("%.2f", totalCost));
-        System.out.println(tipPercent + "%");
-        System.out.println(String.format("%.2f", tipAmount));
-        System.out.println(String.format("%.2f", totalBill));
-        System.out.println(String.format("%.2f", costPerPerson));
-        System.out.println(String.format("%.2f", tipPerPerson));
-        System.out.println(String.format("%.2f", billPerPerson));
+            // cost is cost of food w/o tip
+            // bill is cost + tip
+            double costPerPerson = totalCost / numPeople;
+            double tipAmount = totalCost * tipPercent / 100;
+            double totalBill = tipAmount + totalCost;
+            double tipPerPerson = tipAmount / numPeople;
+            double billPerPerson = costPerPerson + tipPerPerson;
+
+            // https://stackoverflow.com/questions/2538787/how-to-print-a-float-with-2-decimal-places-in-java
+            System.out.println("Total bill before tip: " + String.format("%.2f", totalCost));
+            System.out.println(tipPercent + "%");
+            System.out.println(String.format("%.2f", tipAmount));
+            System.out.println(String.format("%.2f", totalBill));
+            System.out.println(String.format("%.2f", costPerPerson));
+            System.out.println(String.format("%.2f", tipPerPerson));
+            System.out.println(String.format("%.2f", billPerPerson));
+
+            System.out.println("Y/N");
+        } while ("Y".equals(scan.nextLine())); // repeat if user says (Y)es
+
+
+        scan.close();
     }
 }
